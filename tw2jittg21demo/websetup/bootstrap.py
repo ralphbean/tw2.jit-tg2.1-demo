43619f28ec096169722efba83850371e6ee784e4
Ralph Bean <ralph.bean@gmail.com>
2011-03-05 17:19:18 -0500

    Adding random users and groups

--- tw2jittg21demo/websetup/bootstrap.py
+++ tw2jittg21demo/websetup/bootstrap.py
@@ -7,6 +7,41 @@ from tw2jittg21demo import model
 
 import transaction
 
+from random import choice, randint
+
+def add_random_users():
+    """ Add 9 random users """
+    import string
+    chars = string.letters
+    for first in [u'Sally', u'John', u'Tim']:
+        for last in [u'Anderson', u'Flanderson', u'Block']:
+            user = model.User()
+            user.user_name = unicode((first[0] + last).lower())
+            user.display_name = u'%s %s' % (first, last)
+            user.email_address = u'%s@socialistworker.org' % user.user_name
+            user.password = u''.join([choice(chars) for i in range(12)])
+            model.DBSession.add(user)
+
+    model.DBSession.flush()
+
+def add_random_groups():
+    """ Generate a number of random groups and add users to them """
+    for name in ['developer', 'system admin', 'shmeveloper', 'crispin gladbin']:
+        group = model.Group()
+        group.group_name = name
+        group.display_name = (u"%ss group" % name).title()
+        model.DBSession.add(group)
+
+        all_users = model.User.query.all()
+        for i in range(randint(0, len(all_users)-2)):
+            user = choice(all_users)
+            while user in group.users:
+                user = choice(all_users)
+            
+            group.users.append(user)
+
+
+    model.DBSession.flush()
 
 def bootstrap(command, conf, vars):
     """Place any commands to setup tw2jittg21demo here"""
@@ -45,6 +80,10 @@ def bootstrap(command, conf, vars):
     
         model.DBSession.add(u1)
         model.DBSession.flush()
+
+        add_random_users()
+        add_random_groups()
+
         transaction.commit()
     except IntegrityError:
         print 'Warning, there was a problem adding your auth data, it may have already been added:'
